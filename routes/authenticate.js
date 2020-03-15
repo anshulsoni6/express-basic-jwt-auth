faf614fcf0b04d354732cb0c33b5cdb62388be5c
arkenstan <anshulwagadre@gmail.com>
2020-03-15 17:30:26 +0530

    added token signage with JWT

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -4,17 +4,25 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const userModel = require('../models/user.model');
 
+const secret = 'THIS_IS_A_VERY_SIMPLE_SECRET';
+
 router.post('/', async function(req, res) {
 	let authBody = req.body;
 	let findUser = await userModel.findOne({ username: authBody.username }, (err, res) => res);
 
 	let comparePassword = await bcrypt.compareSync(authBody.password, findUser.password, (err, res) => res);
 
-	console.log('COMPARISON', comparePassword);
+	if (comparePassword) {
+		return res.status(200).json({
+			token: jwt.sign({ id: findUser._id }, secret, { expiresIn: '1d' })
+		});
+	} else {
+		return res.status(400).json({
+			error: { message: 'Password is invalid' }
+		});
+	}
 
-	res.json({
-		message: 'AUTH TOKEN'
-	});
+	res.json({});
 });
 
 module.exports = router;
